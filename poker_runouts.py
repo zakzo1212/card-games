d1651b5a109c5a19222237a79896d0cd8b91c343
Zachary Zhang <zakzo1212@gmail.com>
2023-07-30 20:18:40 -0400

    added some logging

--- poker_runouts.py
+++ poker_runouts.py
@@ -1,18 +1,21 @@
 from cards import Deck
 import itertools
+import logging
 
 class HeadsUpRunout:
 
     def __init__(self):
+        logging.basicConfig(level=logging.INFO)
         self.deck = Deck()
         self.board = []
 
     def eval_hand_strength(self, hole):
         '''
-        TODO: evaluate the strength of a hand by the end (find strenght of 7 cards combo)
+        TODO: evaluate the strength of equal hand types relative to each other
         '''
         all_cards = hole + self.board
         best_strength = 0
+        best_cards = []
 
         # Generate all possible combinations of 5 cards
         combinations = itertools.combinations(all_cards, 5)
@@ -21,8 +24,9 @@ class HeadsUpRunout:
             rank = self._eval_hand_strength(combo)
             if rank > best_strength:
                 best_strength = rank
+                best_cards = combo
 
-        return best_strength
+        return best_strength, best_cards
 
     def _eval_hand_strength(self, hand):
 
@@ -76,17 +80,26 @@ class HeadsUpRunout:
         return ranks['high_card']
 
     def _deal_flop(self):
+        logging.info('Dealing the Flop:')
         burn = self.deck.deal_next_card()
         for i in range(3):
             self.board.append(self.deck.deal_next_card())
+        logging.info('Flop comes {} {} {}'.format(self.board[0], self.board[1], self.board[2]))
+        logging.info('BOARD: ' + str(self.get_curr_board()) + '\n')
 
     def _deal_turn(self):
+        logging.info('Dealing the Turn:')
         burn = self.deck.deal_next_card()
         self.board.append(self.deck.deal_next_card())
+        logging.info('Turn comes {}'.format(self.board[3]))
+        logging.info('BOARD: ' + str(self.get_curr_board()) + '\n')
 
     def _deal_river(self):
+        logging.info('Dealing the River')
         burn = self.deck.deal_next_card()
         self.board.append(self.deck.deal_next_card())
+        logging.info('River comes {}'.format(self.board[4]))
+        logging.info('BOARD: ' + str(self.get_curr_board()) + '\n')
 
     def get_curr_board(self):
         return self.board
@@ -94,14 +107,18 @@ class HeadsUpRunout:
     def run(self):
         p1_hole, p2_hole = self.deck.deal_hands(cards_per_hand=2, num_hands=2)
 
+        logging.info('Player 1 hole cards: ' + str(p1_hole))
+        logging.info('Player 2 hole cards: ' + str(p2_hole) + '\n')
+
         self._deal_flop()
         self._deal_turn()
         self._deal_river()
 
-        p1_strength = self.eval_hand_strength(p1_hole)
-        p2_strength = self.eval_hand_strength(p2_hole)
+        p1_strength, p1_best_hand = self.eval_hand_strength(p1_hole)
+        p2_strength, p2_best_hand = self.eval_hand_strength(p2_hole)
 
         print(p1_strength, p2_strength)
+        print(p1_best_hand, p2_best_hand)
 
 
 if __name__ == "__main__":
