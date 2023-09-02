e5b4823baa5f96dc84525bb41658b064f7522381
Zachary Zhang <zakzo1212@gmail.com>
2023-09-02 00:03:02 -0400

    added dealer and player hand execution

--- blackjack.py
+++ blackjack.py
@@ -21,6 +21,22 @@ class Blackjack:
             return 'Face'
         else:
             return 'Number'
+    
+    def _get_hand_value(self, hand):
+        '''
+        returns the value of a blackjack hand
+        '''
+        # TODO: handle aces potentially being 1 or 11
+        value = 0
+        for card in hand:
+            rank = card.get_rank_value()
+            if rank == 14:
+                value += 11
+            elif 14 > rank > 10:
+                value += 10
+            else:
+                value += rank
+        return value
 
     def deal(self):
         self.player_hand, self.dealer_hand = self.deck.deal_hands(2, 2)
@@ -32,7 +48,7 @@ class Blackjack:
         checks if player has a natural blackjack
         '''
         if (self._get_card_type(self.player_hand[0]) == 'Ace' and self._get_card_type(self.player_hand[1]) == 'Face') or \
-            (self._get_card_type(self.player_hand[0]) == 'Ace' and self._get_card_type(self.player_hand[1]) == 'Number'):
+            (self._get_card_type(self.player_hand[1]) == 'Ace' and self._get_card_type(self.player_hand[0]) == 'Face'):
             print('Player has a natural blackjack!')
             return True
         else:
@@ -43,11 +59,48 @@ class Blackjack:
         checks if dealer has a natural blackjack
         '''
         if (self._get_card_type(self.dealer_hand[0]) == 'Ace' and self._get_card_type(self.dealer_hand[1]) == 'Face') or \
-            (self._get_card_type(self.dealer_hand[0]) == 'Ace' and self._get_card_type(self.dealer_hand[1]) == 'Number'):
+            (self._get_card_type(self.dealer_hand[1]) == 'Ace' and self._get_card_type(self.dealer_hand[0]) == 'Face'):
             print('Dealer has a natural blackjack!')
             return True
         else:
             return False
+        
+    def play_hand(self):
+        '''
+        plays the player's hand
+        '''
+        while True:
+            if self.check_player_natural():
+                break
+            else:
+                action = input('Hit or stand? (h/s) ')
+                if action == 'h':
+                    self.player_hand.append(self.deck.deal_next_card())
+                    print('Player hand: {}'.format(self.player_hand))
+                    if self._get_hand_value(self.player_hand) > 21:
+                        print('Player busts!')
+                        break
+                else:
+                    break
+
+    def play_dealer_hand(self):
+        '''
+        plays the dealer's hand
+        '''
+        while True:
+            if self.check_dealer_natural():
+                break
+            else:
+                print('IN HERE')
+                if self._get_hand_value(self.dealer_hand) < 17:
+                    self.dealer_hand.append(self.deck.deal_next_card())
+                    print('Dealer hand: {}'.format(self.dealer_hand))
+                    if self._get_hand_value(self.dealer_hand) > 21:
+                        print('Dealer busts!')
+                        break
+                else:
+                    print('Dealer stands with {}'.format(self._get_hand_value(self.dealer_hand)))
+                    break
 
     def run(self):
         self.deal()
@@ -56,6 +109,7 @@ class Blackjack:
         dealer_natural = self.check_dealer_natural()
 
         self.play_hand()
+        self.play_dealer_hand()
 
 if __name__ == '__main__':
     game = Blackjack()
